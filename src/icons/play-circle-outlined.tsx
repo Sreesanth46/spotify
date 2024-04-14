9d820209ae71e6f1849062defdab5827d4ba320e
Sreesanth <sreesanth46@gmail.com>
2024-04-14 22:02:52 +0530

    chore: added icon

---
+++ src/icons/play-circle-outlined.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const PlayCircleOutlined = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="mr-2 h-4 w-4">
+      <title>play circle outlined</title>
+      <circle cx="12" cy="12" r="10" />
+      <polygon points="10 8 16 12 10 16 10 8" />
+    </svg>
+  );
+};
+
+export default PlayCircleOutlined;
